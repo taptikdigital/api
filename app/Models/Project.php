c978614044cc6efd904804c0108288b1ea2500d4
Md Saddam Hussain <mdsaddamhussain595@gmail.com>
2025-04-20 11:14:09 +0530

    project api samll changes changes

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Carbon;
 
 class Project extends Model
 {
@@ -12,6 +13,26 @@ class Project extends Model
     use SoftDeletes;
 
     protected $fillable = ['user_id', 'subscription_id', 'industry_id', 'name', 'slug', 'whatsapp_number', 'current_status'];
+
+
+    public function getCreatedAtAttribute()
+    {
+        return Carbon::parse($this->attributes['created_at'])
+            
+            ->format('d-M-Y h:i:s A');
+
+
+        // $createdAt = Carbon::parse($this->attributes['created_at']);
+        // $formattedDate = $createdAt->format('d-M-Y h:i:s A');
+
+        // // Subtract 5 hours
+        // $createdAtAgo = $createdAt->subHours(5);
+
+        // return $formattedDate . ' (' . $createdAtAgo->diffForHumans() . ')';
+    }
+
+
+
 
     public function subscription()
     {
